f1855d7331ed78326369426c4085060048011381
Valentina Schüller <valentina.schuller@math.lu.se>
2023-07-10 09:11:45 +0200

    Introduce Groundwater class

--- src/groundwater.py
+++ src/groundwater.py
@@ -2,7 +2,7 @@ import ufl
 from dune.fem.scheme import galerkin
 from dune.fem.space import lagrange
 from dune.grid import structuredGrid
-from dune.ufl import DirichletBC
+from dune.ufl import Constant, DirichletBC
 from mpi4py import MPI  # necessary to avoid ParallelError from DUNE
 
 depth = 1
@@ -13,34 +13,41 @@ h = 3
 K = 1
 c = 1
 
-gridView = structuredGrid([-depth], [0], [20])
+class Groundwater:
+    def __init__(self) -> None:
+        gridView = structuredGrid([-depth], [0], [20])
 
-space = lagrange(gridView, order=1)
-psi_h = space.interpolate(0, name="psi_h")
-psi_h_n = psi_h.copy(name="previous")
+        space = lagrange(gridView, order=1)
+        self.psi_h = space.interpolate(0, name="psi_h")
+        self.psi_h_n = self.psi_h.copy(name="previous")
 
-x = ufl.SpatialCoordinate(space)
-psi = ufl.TrialFunction(space)
-v = ufl.TestFunction(space)
+        x = ufl.SpatialCoordinate(space)
+        psi = ufl.TrialFunction(space)
+        v = ufl.TestFunction(space)
 
-initial = depth / ufl.pi * ufl.sin(ufl.pi / depth * x[0]) + h
-psi_h.interpolate(initial)
+        initial = depth / ufl.pi * ufl.sin(ufl.pi / depth * x[0]) + h
+        self.psi_h.interpolate(initial)
 
-a = (
-    ufl.dot(c * (psi - psi_h_n) / dt, v) + ufl.inner(K * ufl.grad(psi), ufl.grad(v))
-) * ufl.dx
+        a = (
+            ufl.dot(c * (psi - self.psi_h_n) / dt, v) + ufl.inner(K * ufl.grad(psi), ufl.grad(v))
+        ) * ufl.dx
 
-fbnd = (-1 * v * ufl.conditional(x[0] <= (-depth + 1e-8), 1, 0)) * ufl.ds
-# dbc_bottom = DirichletBC(space, 0, x[0] <= (-depth + 1e-8))
-dbc_top = DirichletBC(space, h, x[0] >= (-1e-8))
+        fbnd = (-1 * v * ufl.conditional(x[0] <= (-depth + 1e-8), 1, 0)) * ufl.ds
+        # dbc_bottom = DirichletBC(space, 0, x[0] <= (-depth + 1e-8))
+        dbc_top = DirichletBC(space, h, x[0] >= (-1e-8))
 
-scheme = galerkin([a == fbnd, dbc_top], solver="cg")
-scheme.model.dt = dt
-scheme.model.time = 0
+        self.scheme = galerkin([a == fbnd, dbc_top], solver="cg")
+        self.scheme.model.dt = dt
+        self.scheme.model.time = 0
 
-while scheme.model.time < (t_end - 1e-6):
-    psi_h_n.assign(psi_h)
-    scheme.solve(target=psi_h)
-    scheme.model.time += scheme.model.dt
+    def step(self) -> None:
+        self.psi_h_n.assign(self.psi_h)
+        self.scheme.solve(target=self.psi_h)
+        self.scheme.model.time += self.scheme.model.dt
 
-psi_h.plot()
+groundwater = Groundwater()
+
+while groundwater.scheme.model.time < (t_end - 1e-6):
+    groundwater.step()
+
+groundwater.psi_h.plot()
