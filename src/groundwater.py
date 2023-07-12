bf231a8f7a88e3d968034145d0c7f3820be37320
Valentina Schüller <valentina.schuller@math.lu.se>
2023-07-12 14:30:25 +0200

    Support plotting with ProPlot
    - create x_h to represent coordinate
    using https://www.dune-project.org/sphinx/content/sphinx/dune-fem/geoview_nb.html
    - import proplot and create a simple line plot

--- src/groundwater.py
+++ src/groundwater.py
@@ -1,4 +1,6 @@
+import proplot as pplt
 import ufl
+from dune.fem.function import uflFunction
 from dune.fem.scheme import galerkin
 from dune.fem.space import lagrange
 from dune.grid import structuredGrid
@@ -30,6 +32,10 @@ class Groundwater:
         psi = ufl.TrialFunction(space)
         v = ufl.TestFunction(space)
 
+        # get x-values for plotting/visualization
+        x_func = uflFunction(gridView, name="x", order=1, ufl=x)
+        self.x_h = space.interpolate(x_func)
+
         initial = depth / ufl.pi * ufl.sin(ufl.pi / depth * x[0]) + self.h
         self.psi_h.interpolate(initial)
 
@@ -57,7 +63,11 @@ N = 5
 t_end = N * dt
 groundwater = Groundwater(dt=dt)
 
+fig, ax = pplt.subplots()
+ax.plot(groundwater.x_h.as_numpy, groundwater.psi_h.as_numpy, marker=".", color="k")
+
 while groundwater.scheme.model.time < (t_end - 1e-6):
     groundwater.step()
+    ax.plot(groundwater.x_h.as_numpy, groundwater.psi_h.as_numpy, marker=".")
 
-groundwater.psi_h.plot()
+pplt.show()
