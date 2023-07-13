c01e6e23535a3f1697dabbb19fb0e5ea1dc496c6
Valentina Schüller <valentina.schuller@math.lu.se>
2023-07-13 14:05:10 +0200

    Encapsulate more functionality in River class
    this will make it more consistent with Groundwater
    and the interface more reusable for 2D case
    (cf. https://precice.org/couple-your-code-preparing-your-solver.html)

--- src/river.py
+++ src/river.py
@@ -3,11 +3,19 @@ import proplot as pplt
 
 
 class River:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, h_0: float, t_0: float) -> None:
+        self.h = h_0
+        self.t = t_0
+        self.result = [h_0]
+        self.time = [t_0]
 
-    def step(self, dt: float, h: float, flux: float) -> float:
-        return h + dt * flux
+    def step(self, dt: float, flux: float) -> float:
+        self.h += dt * flux
+        self.t += dt
+
+    def end_time_step(self) -> None:
+        self.result.append(self.h)
+        self.time.append(self.t)
 
 
 t = 0
@@ -16,21 +24,13 @@ N = 20
 dt = (t_end - t) / N
 h_0 = 1
 
-h = h_0
+river = River(h_0, t)
 
-h_values = [h_0]
-t_values = [t]
-
-river = River()
-
-while t < t_end:
+while river.t < t_end:
     flux = -1 + 2 * np.random.rand()
-    h = river.step(dt, h, flux)
-    t += dt
+    river.step(dt, flux)
+    river.end_time_step()
 
-    h_values.append(h)
-    t_values.append(t)
-
-fig, ax = pplt.subplots()
-ax.plot(t_values, h_values)
+_, ax = pplt.subplots()
+ax.plot(river.time, river.result)
 pplt.show()
