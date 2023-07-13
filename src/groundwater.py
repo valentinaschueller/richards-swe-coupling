86a4b240f17fbe524e8fc7168a2c50d539353426
Valentina Schüller <valentina.schuller@math.lu.se>
2023-07-13 16:18:37 +0200

    First working version of preCICE coupling
    followed https://github.com/BenjaminRodenberg/oscillator-example/blob/main/precice-2/configs/precice-config-0.000625.xml
    and
    https://precice.org/couple-your-code-mesh-and-data-access.html

--- src/groundwater.py
+++ src/groundwater.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+
+import numpy as np
+import precice
 import proplot as pplt
 import ufl
 from dune.fem.function import uflFunction
@@ -18,8 +22,8 @@ class Groundwater:
     ) -> None:
         gridView = structuredGrid([-depth], [0], [20])
 
-        space = lagrange(gridView, order=1)
-        self.psi_h = space.interpolate(0, name="psi_h")
+        self.space = lagrange(gridView, order=1)
+        self.psi_h = self.space.interpolate(0, name="psi_h")
         self.psi_h_n = self.psi_h.copy(name="previous")
 
         self.c = Constant(c, name="c")
@@ -27,13 +31,13 @@ class Groundwater:
         self.dt = Constant(dt, name="dt")
         self.h = Constant(h, name="h")
 
-        x = ufl.SpatialCoordinate(space)
-        psi = ufl.TrialFunction(space)
-        v = ufl.TestFunction(space)
+        x = ufl.SpatialCoordinate(self.space)
+        psi = ufl.TrialFunction(self.space)
+        v = ufl.TestFunction(self.space)
 
         # get x-values for plotting/visualization
         x_func = uflFunction(gridView, name="x", order=1, ufl=x)
-        self.x_h = space.interpolate(x_func)
+        self.x_h = self.space.interpolate(x_func)
 
         initial = depth / ufl.pi * ufl.sin(ufl.pi / depth * x[0]) + self.h
         self.psi_h.interpolate(initial)
@@ -45,28 +49,67 @@ class Groundwater:
 
         fbnd = (-1 * v * ufl.conditional(x[0] <= (-depth + 1e-8), -1, 0)) * ufl.ds
         # dbc_bottom = DirichletBC(space, 0, x[0] <= (-depth + 1e-8))
-        dbc_top = DirichletBC(space, self.h, x[0] >= (-1e-8))
+        dbc_top = DirichletBC(self.space, self.h, x[0] >= (-1e-8))
 
         self.scheme = galerkin([a == fbnd, dbc_top], solver="cg")
         self.scheme.model.dt = dt
         self.scheme.model.time = 0
+        self.result = self.psi_h.as_numpy[np.newaxis]  # add a dimension
+        self.time = [0]
+        self.flux = 0
 
-    def step(self) -> None:
+    def step(self, dt: float) -> None:
         self.psi_h_n.assign(self.psi_h)
+        self.scheme.model.dt = dt
+        self.dt.value = dt
         self.scheme.solve(target=self.psi_h)
         self.scheme.model.time += self.scheme.model.dt
+        grad_psi_sfc = self.space.interpolate(ufl.grad(self.psi_h)).as_numpy[-1]
+        self.flux = self.K.value * (grad_psi_sfc + 1)
+        print(f"{self.flux=}")
+
+    def end_time_step(self) -> None:
+        self.result = np.append(self.result, self.psi_h.as_numpy[np.newaxis], axis=0)
+        self.time.append(self.scheme.model.time)
 
 
-dt = 1e-2
-N = 5
-t_end = N * dt
-groundwater = Groundwater(dt=dt)
+participant_name = "GroundwaterSolver"
+config_file = Path("precice-config.xml")
+solver_process_index = 0
+solver_process_size = 1
+interface = precice.Interface(
+    participant_name, str(config_file), solver_process_index, solver_process_size
+)
 
-fig, ax = pplt.subplots()
-ax.plot(groundwater.x_h.as_numpy, groundwater.psi_h.as_numpy, marker=".", color="k")
+mesh_name = "GroundwaterMesh"
+mesh_id = interface.get_mesh_id(mesh_name)
+dimensions = interface.get_dimensions()
+vertex = np.zeros(dimensions)
+vertex_id = interface.set_mesh_vertex(mesh_id, vertex)
 
-while groundwater.scheme.model.time < (t_end - 1e-6):
-    groundwater.step()
-    ax.plot(groundwater.x_h.as_numpy, groundwater.psi_h.as_numpy, marker=".")
+data_id_h = interface.get_data_id("Height", mesh_id)
+data_id_v = interface.get_data_id("Flux", mesh_id)
 
+t = 0
+t_end = 1
+N = 10
+solver_dt = (t_end - t) / N
+groundwater = Groundwater(dt=solver_dt)
+
+precice_dt = interface.initialize()
+interface.initialize_data()
+while interface.is_coupling_ongoing():
+    # solver_dt = groundwater.begin_time_step()
+    dt = min(solver_dt, precice_dt)
+    groundwater.step(dt)
+
+    groundwater.h.value = interface.read_scalar_data(data_id_h, vertex_id)
+
+    interface.write_scalar_data(data_id_v, vertex_id, groundwater.flux)
+    precice_dt = interface.advance(dt)
+    groundwater.end_time_step()
+interface.finalize()
+
+_, ax = pplt.subplots()
+ax.plot(groundwater.x_h.as_numpy, groundwater.result.T, marker=".")
 pplt.show()
--- src/river.py
+++ src/river.py
@@ -1,4 +1,7 @@
+from pathlib import Path
+
 import numpy as np
+import precice
 import proplot as pplt
 
 
@@ -18,18 +21,51 @@ class River:
         self.time.append(self.t)
 
 
+participant_name = "RiverSolver"
+config_file = Path("precice-config.xml")
+solver_process_index = 0
+solver_process_size = 1
+interface = precice.Interface(
+    participant_name, str(config_file), solver_process_index, solver_process_size
+)
+
+mesh_name = "RiverMesh"
+mesh_id = interface.get_mesh_id(mesh_name)
+dimensions = interface.get_dimensions()
+vertex = np.zeros(dimensions)
+vertex_id = interface.set_mesh_vertex(mesh_id, vertex)
+
+data_id_h = interface.get_data_id("Height", mesh_id)
+data_id_v = interface.get_data_id("Flux", mesh_id)
+
 t = 0
 t_end = 1
-N = 20
-dt = (t_end - t) / N
+N = 10
+solver_dt = (t_end - t) / N
 h_0 = 1
 
 river = River(h_0, t)
 
-while river.t < t_end:
-    flux = -1 + 2 * np.random.rand()
+precice_dt = interface.initialize()
+
+if interface.is_action_required(precice.action_write_initial_data()):
+    interface.write_scalar_data(data_id_v, vertex_id, 0)
+    interface.write_scalar_data(data_id_h, vertex_id, h_0)
+    interface.mark_action_fulfilled(precice.action_write_initial_data())
+
+interface.initialize_data()
+
+while interface.is_coupling_ongoing():
+    # solver_dt = river.begin_time_step()
+    dt = min(solver_dt, precice_dt)
+    read_data = interface.read_scalar_data(data_id_v, vertex_id)
+    flux = read_data
     river.step(dt, flux)
+    write_data = river.h
+    interface.write_scalar_data(data_id_h, vertex_id, write_data)
+    precice_dt = interface.advance(dt)
     river.end_time_step()
+interface.finalize()
 
 _, ax = pplt.subplots()
 ax.plot(river.time, river.result)
