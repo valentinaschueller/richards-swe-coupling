Fix weak form and flux computation in Groundwater
Now I *actually* understood how DUNE builds the matrix
-> had to adjust the weak form and flux computation
to be consistent with the analysis

src/groundwater.py
@@ -39,8 +39,8 @@ class Groundwater:
         self.psi_h_n = self.psi_h.copy(name="previous")
 
         a = (
-            ufl.dot(self.c * (psi - self.psi_h_n) / self.dt, v)
-            + ufl.inner(self.K * ufl.grad(psi), ufl.grad(v))
+            ufl.dot(self.c * (psi - self.psi_h_n), v)
+            + self.dt * ufl.inner(self.K * ufl.grad(psi), ufl.grad(v))
         ) * ufl.dx
 
         # Set boundary conditions
@@ -70,11 +70,15 @@ class Groundwater:
         self._compute_flux()
 
     def _compute_flux(self) -> None:
-        M = (self.c.value / 6) * np.array([1, 2])
-        A = self.K.value * self.dt.value / self.dz**2 * np.array([-1, 1])
+        M = (self.c.value * self.dz / 6) * np.array([1, 2])
+        A = self.K.value / self.dz * np.array([-1, 1])
         psi_now = self.psi_h.as_numpy[-2:]
         psi_n = self.psi_h_n.as_numpy[-2:]
-        v_s = -(np.dot(M, (psi_now - psi_n)) + np.dot(A, psi_now) + self.K.value)
+        v_s = -(
+            np.dot(M, (psi_now - psi_n)) / self.dt.value
+            + np.dot(A, psi_now)
+            + self.K.value
+        )
         self.flux = v_s
 
     def solve(self, dt: float) -> None: