e4b5422588535346c107d96c6398b3bf629a8a0a
Valentina Schüller <valentina.schuller@math.lu.se>
2023-08-21 16:15:23 +0200

    Add test for BoundaryCondition sensitivity
    also explicitly set omega for the reference case in test_full_coupling()

--- tests/test_full_coupling.py
+++ tests/test_full_coupling.py
@@ -1,6 +1,6 @@
 from coupling.analysis import compute_coupling_behavior
 from coupling.process_results import compute_convergence_rate
-from coupling.setup_simulation import load_params
+from coupling.setup_simulation import BoundaryCondition, load_params
 from coupling.simulation import run_coupled_simulation
 
 convergence_rate = None
@@ -10,6 +10,7 @@ omega_opt = None
 def test_full_coupling():
     yaml_file = "tests/test_params.yaml"
     params = load_params(yaml_file)
+    params.omega = 1
 
     run_coupled_simulation(params)
 
@@ -26,3 +27,34 @@ def test_full_coupling():
     run_coupled_simulation(params)
     optimal_cvg_rate = compute_convergence_rate(cvg_logfile)
     assert optimal_cvg_rate < 1e-8
+
+
+def test_bc_sensitivity():
+    yaml_file = "tests/test_params.yaml"
+    params = load_params(yaml_file)
+    cvg_logfile = "precice-RiverSolver-convergence.log"
+
+    S = compute_coupling_behavior(params.K, params.c, params.L, params.dt, params.M)[1]
+    analytical_cvg_rate = abs(S)
+
+    params.bc_type = BoundaryCondition.dirichlet
+    params.dirichlet_value = 0.0
+    run_coupled_simulation(params)
+    cvg_rate_hom_dbc = compute_convergence_rate(cvg_logfile)
+
+    params.dirichlet_value = 10.0
+    run_coupled_simulation(params)
+    cvg_rate_nhom_dbc = compute_convergence_rate(cvg_logfile)
+
+    params.bc_type = BoundaryCondition.no_flux
+    run_coupled_simulation(params)
+    cvg_rate_no_flux = compute_convergence_rate(cvg_logfile)
+
+    params.bc_type = BoundaryCondition.free_drainage
+    run_coupled_simulation(params)
+    cvg_rate_free_drainage = compute_convergence_rate(cvg_logfile)
+
+    assert abs(cvg_rate_hom_dbc - analytical_cvg_rate) < 1e-8
+    assert abs(cvg_rate_nhom_dbc - analytical_cvg_rate) < 1e-8
+    assert abs(cvg_rate_free_drainage - analytical_cvg_rate) < 1e-1
+    assert abs(cvg_rate_no_flux - analytical_cvg_rate) < 1e-1
